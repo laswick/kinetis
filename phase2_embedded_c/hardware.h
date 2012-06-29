7fcf06e2be224e1a83262becb163cfd96154518f
Robert Laswick <robert.laswick@l-3com.com>
2012-06-29 13:14:03 -0400

    Tower CCA hardware defines will now reside in hardware.h.

--- phase2_embedded_c/hardware.h
+++ phase2_embedded_c/hardware.h
@@ -46,5 +46,25 @@ int32_t featureInit(featureConfig_t *cfg);
 int32_t featureWrite(uint8_t *buffer, int32_t len);
 int32_t featureRead(uint8_t *buffer, int32_t len);
 
-/******************************************************************************/
+/*******************************************************************************
+*
+* CCA Hardware Defines
+*
+*******************************************************************************/
+#if defined(FREESCALE_K60N512_TOWER)
 
+/* LEDS ***********************************************************************/
+
+#define N_LED_ORANGE_PORT PORTA
+#define N_LED_ORANGE_PIN  11
+
+#define N_LED_YELLOW_PORT PORTA
+#define N_LED_YELLOW_PIN  28
+
+#define N_LED_GREEN_PORT  PORTA
+#define N_LED_GREEN_PIN   29
+
+#define N_LED_BLUE_PORT   PORTA
+#define N_LED_BLUE_PIN    10
+
+#endif
