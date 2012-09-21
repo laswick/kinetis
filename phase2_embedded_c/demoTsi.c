6a22c978b43e8573cb054ca3815320de592afd52
Robert Laswick <robert.laswick@gmail.com>
2012-09-20 21:36:00 -0400

    See last commit message...

--- phase2_embedded_c/demoTsi.c
+++ phase2_embedded_c/demoTsi.c
@@ -5,6 +5,13 @@
 * David Kennedy
 * July 15 2012
 *
+* Copyright (C) 2012 www.laswick.net
+*
+* This program is free software.  It comes without any warranty, to the extent
+* permitted by applicable law.  You can redistribute it and/or modify it under
+* the terms of the WTF Public License (WTFPL), Version 2, as published by
+* Sam Hocevar.  See http://sam.zoy.org/wtfpl/COPYING for more details.
+*
 *******************************************************************************/
 #include <stdlib.h>
 #include <stdio.h>
