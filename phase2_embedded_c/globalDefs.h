d53b13573e94b9dd70f90efa76f3dda1f8ae07ea
David Kennedy <dj2kenne@alumni.uwaterloo.ca>
2012-10-31 21:12:46 -0400

    Make uint32_t and int32_t definitions align with stdint definitions.

--- phase2_embedded_c/globalDefs.h
+++ phase2_embedded_c/globalDefs.h
@@ -22,11 +22,11 @@
 
 typedef unsigned char  uint8_t;
 typedef unsigned short uint16_t;
-typedef unsigned int   uint32_t;
+typedef unsigned long  uint32_t;
 
 typedef signed char    int8_t;
 typedef signed short   int16_t;
-typedef signed int     int32_t;
+typedef signed long    int32_t;
 
 typedef unsigned char  bool8_t;
 typedef unsigned short bool16_t;
